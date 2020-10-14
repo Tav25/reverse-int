30c2ab2d66ed9cfe7f68959c4307ac011169c473
Turbin Alex <turbin28@gmail.com>
2020-10-14 18:42:48 +0300

    feat: revers-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    cl(n)
+    const polovina = (Math.floor(n.toString().length / 2))
+    let str = n.toString().replace(/(-)/g, "");
+    newStr = reverseString(str.substr(polovina + 1, n.length)) + str[polovina] + reverseString(str.substr(0, polovina))
+    console.log(newStr);
+    return newStr
 }
+
+function cl(x) { console.log(x) }
+function reverseString(str) { return str.split("").reverse().join("") }
